6cefc077f54ba54cf866e4c1845ecf8effd28699
Ryan Yogan <ryanyogan@gmail.com>
2012-11-24 18:13:16 -0600

    Add validation to ensure names are specified when creating projects

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.9'
 gem 'sqlite3'
 gem 'jquery-rails'
+gem 'dynamic_form', '1.1.4'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- spec/integration/creating_projects_spec.rb
+++ spec/integration/creating_projects_spec.rb
@@ -2,9 +2,18 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do
-  scenario 'can create a project' do
+  before do
     visit '/'
-    click_link 'New Project'
+    click_link "New Project"
+  end
+
+  scenario "can not create a project without a name" do
+    click_button 'Create Project'
+    page.should have_content("Project has not been created.")
+    page.should have_content("Name can't be blank")
+  end
+
+  scenario 'can create a project' do
     fill_in 'Name', with: 'TextMate 2'
     fill_in 'Description', with: 'A text-editor for OS X'
     click_button 'Create Project'
